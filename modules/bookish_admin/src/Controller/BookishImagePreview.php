6268db71941a990c5c1ff280bfa90c385868c45e
Sam Mortenson <samuel@mortenson.coffee>
2022-01-07 15:35:36 -0800

    More better

---
+++ modules/bookish_admin/src/Controller/BookishImagePreview.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Drupal\bookish_admin\Controller;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\StreamWrapper\StreamWrapperManager;
+use Drupal\file\FileInterface;
+use Drupal\image\ImageStyleInterface;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @todo
+ */
+class BookishImagePreview extends ControllerBase {
+
+  /**
+   * @todo
+   */
+  public function build(FileInterface $file, ImageStyleInterface $image_style, Request $request) {
+    /** @var \Drupal\Core\File\FileSystemInterface $file_system */
+    $file_system = \Drupal::service('file_system');
+    /** @var \Drupal\Core\Image\ImageFactory $image_factory */
+    $image_factory = \Drupal::service('image.factory');
+    $original_image_data = json_decode($file->bookish_image_data->getString(), TRUE);
+    $new_image_data = json_decode($request->query->get('bookish_image_data', []), TRUE);
+    $image_data = array_merge($original_image_data, $new_image_data);
+    $file->bookish_image_data = json_encode($image_data);
+    // @todo Make more unique.
+    $derivative_uri = 'public://bookish-image-preview/' . $file->getFileUri();
+    $file_system->delete($derivative_uri);
+    $image_style->createDerivative($file->getFileUri(), $derivative_uri);
+    $image = $image_factory->get($derivative_uri);
+    $headers = [
+      'Content-Type' => $image->getMimeType(),
+      'Content-Length' => $image->getFileSize(),
+    ];
+    return new BinaryFileResponse($derivative_uri, 200, $headers, FALSE);
+  }
+
+  public function access(FileInterface $file, ImageStyleInterface $image_style) {
+    $uri = $file->getFileUri();
+    $scheme = StreamWrapperManager::getScheme($uri);
+    return AccessResult::allowedIf(file_exists($uri) && $file->access('download') && $scheme !== 'private');
+  }
+
+}
--- modules/bookish_admin/src/Plugin/Field/FieldWidget/BookishImageWidget.php
+++ modules/bookish_admin/src/Plugin/Field/FieldWidget/BookishImageWidget.php
@@ -5,6 +5,7 @@ namespace Drupal\bookish_admin\Plugin\Field\FieldWidget;
 use Drupal\Component\Utility\NestedArray;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Render\Element;
+use Drupal\Core\Url;
 use Drupal\image\Plugin\Field\FieldWidget\ImageWidget;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -31,7 +32,7 @@ class BookishImageWidget extends ImageWidget {
     $item['fids'] = $element['fids']['#value'];
 
     $preview_id = 'bookish-image-preview-' . $element['#field_name'] . '-' . $element['#delta'];
-    $element['brightness'] = [
+    $element['bookish_image_data']['brightness'] = [
       '#title' => t('Brightness'),
       '#type' => 'range',
       '#min' => -255,
@@ -52,7 +53,7 @@ class BookishImageWidget extends ImageWidget {
     if (!empty($element['#files'])) {
       $file = reset($element['#files']);
       $image_data = json_decode($file->bookish_image_data->getString(), TRUE);
-      $element['brightness']['#default_value'] = $image_data['brightness'];
+      $element['bookish_image_data']['brightness']['#default_value'] = $image_data['brightness'];
     }
 
     $element['preview']['#prefix'] = '<div id="' . $preview_id . '">';
@@ -65,27 +66,25 @@ class BookishImageWidget extends ImageWidget {
     $form_parents = explode('/', $request->query->get('element_parents'));
     $form_parents = array_filter($form_parents, [Element::class, 'child']);
     $element = NestedArray::getValue($form, $form_parents);
-    if (!empty($element['#files'])) {
-      /** @var \Drupal\file\FileInterface $file */
-      $file = reset($element['#files']);
-      $image_data = json_decode($file->bookish_image_data->getString(), TRUE);
-      $image_data['brightness'] = $element['brightness']['#value'];
-      $file->bookish_image_data = json_encode($image_data);
-      /** @var \Drupal\image\ImageStyleInterface $image_style */
-      $image_style = \Drupal::entityTypeManager()->getStorage('image_style')->load($element['preview']['#style_name']);
-      if ($image_style) {
-        /** @var \Drupal\Core\File\FileUrlGeneratorInterface $file_url_generator */
-        $file_url_generator = \Drupal::service('file_url_generator');
-        /** @var \Drupal\Core\File\FileSystemInterface $file_system */
-        $file_system = \Drupal::service('file_system');
-        // @todo Make more unique.
-        $derivative_uri = 'public://bookish-image-preview/' . $file->getFileUri();
-        $file_system->delete($derivative_uri);
-        $image_style->createDerivative($file->getFileUri(), $derivative_uri);
-        $element['preview']['#theme'] = 'image';
-        $element['preview']['#uri'] = $file_url_generator->generate($derivative_uri)->toString() . '?cache_bypass=' . md5($file->bookish_image_data->getString());
-      }
+    if (empty($element['#files'])) {
+      return $element['preview'];
     }
+
+    /** @var \Drupal\file\FileInterface $file */
+    $file = reset($element['#files']);
+    $image_data = json_decode($file->bookish_image_data->getString(), TRUE);
+    $new_image_data = $form_state->getValue(array_merge($element['#parents'], ['bookish_image_data']));
+    $image_data = array_merge($image_data, $new_image_data);
+
+    $element['preview']['#theme'] = 'image';
+    $element['preview']['#uri'] = Url::fromRoute('bookish_image_preview', [
+      'file' => $file->id(),
+      'image_style' => $element['preview']['#style_name'],
+    ], [
+      'query' => [
+        'bookish_image_data' => json_encode($image_data),
+      ],
+    ])->toString();
     return $element['preview'];
   }
 
